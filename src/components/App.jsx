03216e0b5ed50499d97f54731ea1836639576152
ciplator <ciplator@gmail.com>
2023-12-26 22:28:56 +0200

    all

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+
+      <button type="button" className="my-button">my first button</button>
+
+
+      <p>React homework template</p> 
     </div>
   );
 };

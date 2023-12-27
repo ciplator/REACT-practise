70ebe31304ba8abff631214a15eacd031039bfc6
ciplator <ciplator@gmail.com>
2023-12-27 21:00:58 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,12 @@ export const App = () => {
   return (
     <div>
 
+
+
       <button type="button" className="my-button">my first button</button>
 
 
-      <p>React homework template</p> 
+      <p>React homework template</p>
     </div>
   );
 };

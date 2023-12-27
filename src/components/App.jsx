490104a0f489a0f9f1b18548dcf2cfaf5f1830e4
ciplator <ciplator@gmail.com>
2023-12-27 21:27:20 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,8 +6,26 @@ export const App = () => {
 
       <button type="button" className="my-button">my first button</button>
 
+<Product name = "taco with lime" price = "10.99"/>
+<Product name = "taco L" price = "8.99"/>
+<Product name = "taco M" price = "6.99"/>
+
 
       <p>React homework template</p>
     </div>
   );
 };
+const Product = ({name, price}) => {
+  return (
+    <div>
+      <img src="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
+       alt="product" />
+
+        <h3>{name}</h3>
+        <p>Product description</p>
+          <p>Price: {price}</p>
+
+    </div>
+  );
+}
+
